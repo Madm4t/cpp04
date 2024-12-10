9d51c04108979674bd38c73f42bd52ad0d5c5fef
matvej <mgering@mail.de>
2024-12-10 18:46:36 +0100

    ex03 not finished

--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -12,17 +12,15 @@ protected:
 	std::string const type;
 
 public:
+	AMateria();
 	AMateria(std::string const & type);
+	AMateria(const AMateria& other);
+	AMateria& operator=(const AMateria& other);
 	virtual ~AMateria();
 
 	std::string const & getType() const; //Returns the materia type
 	virtual AMateria* clone() const = 0;
 	virtual void use(ICharacter& target);
-
-private:
-	AMateria();
-	AMateria(const AMateria& other);
-	AMateria& operator=(const AMateria& other);
 };
 
 #endif
